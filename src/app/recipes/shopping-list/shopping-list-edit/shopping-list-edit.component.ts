af3adb4cab5f745bb4a25b16891bbd584e946c91
Rudi VanLitsenborgh <Rudi VanLitsenborgh@Unipro2.local>
2018-11-19 17:28:28 +0000

    remove wrong structure and fix correct path

--- src/app/recipes/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/recipes/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-shopping-list-edit',
+  selector: 'app-shopping-edit',
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.css']
 })
